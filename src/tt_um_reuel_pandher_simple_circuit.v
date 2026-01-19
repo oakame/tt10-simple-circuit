3898e65af4f02629134d3abcfe49bd2a1004731e
oakame <reuellpandher@gmail.com>
2026-01-19 17:47:47 -0600

    Update tt_um_reuel_pandher_simple_circuit.v

--- src/tt_um_reuel_pandher_simple_circuit.v
+++ src/tt_um_reuel_pandher_simple_circuit.v
@@ -4,7 +4,7 @@
  */
 
 `default_nettype none
-module tt_um_islam_ihfaz_simple_circuit (
+module tt_um_reuel_pandher_simple_circuit (
 input wire [7:0] ui_in, // Dedicated inputs
 output wire [7:0] uo_out, // Dedicated outputs
 input wire [7:0] uio_in, // IOs: Input path
